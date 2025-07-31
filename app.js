4612a9ad24a0c5fcdbc032c0c6ec3b8eab52712b
Rockio <berlin.operators@gmail.com>
2025-07-31 12:58:28 +0300

    fixing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the homepage
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
